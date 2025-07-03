e8acb6ef149cbc85aec027184a395262385a1dd8
yadav412 <ysa306@sfu.ca>
2025-07-02 22:20:20 -0700

    feat: created the frontend skeleton for tracemoe feature bed

--- main js.js
+++ main js.js
@@ -1,15 +1,7 @@
 
-    let prevScrollPos = window.pageYOffset;
 
-    window.onscroll = function () {
-      const currentScrollPos = window.pageYOffset;
-      const navbar = document.getElementById("navbar");
 
-      if (prevScrollPos > currentScrollPos) {
-        navbar.style.top = "0";
-      } else {
-        navbar.style.top = "-80px";
-      }
+function onadd(){
+    
 
-      prevScrollPos = currentScrollPos;
-    };
+}
