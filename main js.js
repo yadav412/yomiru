b20d03b8187303fef1d1884fb5732b20ebcb1cc7
yadav412 <ysa306@sfu.ca>
2025-07-06 20:34:03 -0700

    feat : added the remaining pages as well as added the rough navbar

--- main js.js
+++ main js.js
@@ -1,7 +1,11 @@
 
-
-
-function onadd(){
-    
-
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
 }
